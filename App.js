5ca72364e3573ea0f0d62ce2c5de03dc3cc5ff6f
faysalsaade <faysalsaadeh@gmail.com>
2025-04-10 22:25:49 +0300

    first commit

--- App.js
+++ App.js
@@ -2,16 +2,23 @@ import { StyleSheet } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import CategoriesScreen from "./screens/CategoriesScreen";
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
       <NavigationContainer>
-        <CategoriesScreen />;
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        </Stack.Navigator>
+        <CategoriesScreen />
       </NavigationContainer>
     </>
   );
 }
-const styles = StyleSheet.create({
-  container: {},
-});
+// const styles = StyleSheet.create({
+//   container: {},
+// });
