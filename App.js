afdbb2a19433824dd6bbb471e145e5f4d78af782
faysalsaade <faysalsaadeh@gmail.com>
2025-05-04 23:02:44 +0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!!!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <NavigationContainer></NavigationContainer>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
