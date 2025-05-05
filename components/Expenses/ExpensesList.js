b30e7113c7077006cac366ea724bcea0ccb7916f
faysalsaade <faysalsaadeh@gmail.com>
2025-05-05 21:32:40 +0300

    first commit

---
+++ components/Expenses/ExpensesList.js
@@ -0,0 +1,6 @@
+import { FlatList } from "react-native-gesture-handler";
+
+function ExpensesList() {
+  return <FlatList />;
+}
+export default ExpensesList;
--- components/Expenses/ExpensesOutput.js
+++ components/Expenses/ExpensesOutput.js
@@ -1,14 +1,40 @@
-import { View, Text, FlatList } from "react-native";
+import { View } from "react-native";
+import ExpensesSummary from "./ExpensesSummary";
+import ExpensesList from "./ExpensesList";
 
-function ExpenseOutPut({ expenses }) {
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    description: "A pair of Shoes",
+    amount: 59.99,
+    date: new Date("2021-12-19"),
+  },
+  {
+    id: "e2",
+    description: "A pair of trousers",
+    amount: 89.29,
+    date: new Date("2022-01-5"),
+  },
+  {
+    id: "e3",
+    description: "Some bananas",
+    amount: 5.99,
+    date: new Date("2021-12-01"),
+  },
+  {
+    id: "e4",
+    description: "A book",
+    amount: 14.99,
+    date: new Date("2022-2-19"),
+  },
+];
+
+function ExpensesOutPut({ expenses, expensesPeriod }) {
   return (
     <View>
-      <View>
-        <Text>Last 7 days</Text>
-        <Text>$177.95</Text>
-      </View>
-      <FlatList />
+      <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
+      <ExpensesList />
     </View>
   );
 }
-export default ExpenseOutPut;
+export default ExpensesOutPut;
