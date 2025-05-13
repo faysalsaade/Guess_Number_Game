3a366663e83dcaaedceee3f1176025de6a24c8bf
faysalsaade <faysalsaadeh@gmail.com>
2025-05-13 17:11:12 +0300

    first commit

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,6 +1,15 @@
+import { useLayoutEffect } from "react";
 import { Text } from "react-native";
 
-function ManageExpense() {
+function ManageExpense({ route, navigation }) {
+  const editedExpenseId = route.params?.expenseId;
+  const isEditing = !!editedExpenseId;
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "AddExpense",
+    });
+  }, [navigation, isEditing]);
+
   return <Text>ManageExpense</Text>;
 }
 export default ManageExpense;
