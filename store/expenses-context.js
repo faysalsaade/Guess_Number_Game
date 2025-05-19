d04433715c1f8b65d55f6454736c725c13a68496
faysalsaade <faysalsaadeh@gmail.com>
2025-05-19 20:51:38 +0300

    first commit

--- store/expenses-context.js
+++ store/expenses-context.js
@@ -1,6 +1,34 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 export const ExpensesContext = createContext({
   expenses: [],
-  addExpense: () => {},
-  deleteExpense: () => {},
+  addExpense: ({ description, amount, date }) => {},
+  deleteExpense: (id) => {},
+  updateExpense: (id, { description, amount, date }) => {},
 });
+
+function expensesReducer(state, action) {
+  switch (action.type) {
+    case "ADD":
+      return [{}, ...state];
+    case "UPDATE":
+    case "DELETE":
+    default:
+      return state;
+  }
+}
+
+function ExpensesContextProvider({ children }) {
+  const [expensesState, dispatch] = useReducer(expensesReducer);
+  function addExpense(expenseData) {
+    dispatch({ type: "ADD", payload: expenseData });
+  }
+  function deleteExpense(id) {
+    dispatch({ type: "DELETE", payload: id });
+  }
+  function updateExpense(id, expenseData) {
+    dispatch({ type: "UPDATE", payload: { id: id, expenseData: expenseData } });
+  }
+
+  return <ExpensesContext.Provider>{children}</ExpensesContext.Provider>;
+}
+export default ExpensesContextProvider;
