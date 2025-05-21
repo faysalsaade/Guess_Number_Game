ebc395f936805edc333f98b3aa67c9f767039761
faysalsaade <faysalsaadeh@gmail.com>
2025-05-21 20:25:06 +0300

    first commit

---
+++ components/ManageExpense/input.js
@@ -0,0 +1,11 @@
+import { View, TextInput, Text } from "react-native";
+
+function Input({ label, textInputConfig }) {
+  return (
+    <View>
+      <Text>{label}</Text>
+      <TextInput {...textInputConfig} />
+    </View>
+  );
+}
+export default Input;
--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -4,6 +4,8 @@ import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/styles";
 import Button from "../components/UI/Button";
 import { ExpensesContext } from "../store/expenses-context";
+import ExpenseForm from "../components/ManageExpense/ExpenseForm";
+
 function ManageExpense({ route, navigation }) {
   const expensesCtx = useContext(ExpensesContext);
   const editedExpenseId = route.params?.expenseId;
@@ -39,6 +41,7 @@ function ManageExpense({ route, navigation }) {
   }
   return (
     <View style={styles.container}>
+      <ExpenseForm />
       <View style={styles.buttons}>
         <Button style={styles.button} mode="flat" onPress={cancelHandler}>
           Cancel
