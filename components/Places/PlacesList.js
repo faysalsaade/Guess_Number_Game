a7c11ea58e84ecb27279bfaf5e83dbc0758b7d58
faysalsaade <faysalsaadeh@gmail.com>
2025-07-23 21:43:19 +0300

    first commit

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -1,7 +1,28 @@
 import { FlatList } from "react-native-gesture-handler";
+import PlaceItem from "./placeItem";
+import { View, StyleSheet } from "react-native";
 
-function PlaceList({places}) {
-return <FlatList data={places} keyExtractor={item => item.id} renderItem={}  />;
+function PlaceList({ places }) {
+  if (!places || places.length === 0) {
+    return (
+      <View>
+        <Text>No places added yet - start adding some!</Text>
+      </View>
+    );
+  }
+  return (
+    <FlatList
+      data={places}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => <PlaceItem place={item} />}
+    />
+  );
 }
-export default PlaceList;   
-
+export default PlaceList;
+const styles = StyleSheet.create({
+  fallbackContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
