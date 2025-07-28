39864c9dff280e164524a1ea8015d8c1468f698f
faysalsaade <faysalsaadeh@gmail.com>
2025-07-28 21:32:03 +0300

    first commit

--- App.js
+++ App.js
@@ -4,21 +4,27 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import AllPlaces from "./screens/AllPlaces";
 import AddPlace from "./screens/AddPlace";
 import IconButton from "./components/UI/IconButton";
+import { Colors } from "./components/constants/colors";
+
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <>
       <StatusBar style="dark" />
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: Colors.primary500 },
+            headerTintColor: Colors.gray700,
+            contentStyle: { backgroundColor: Colors.gray700 },
+          }}
+        >
           <Stack.Screen
             name="AllPlaces"
             component={AllPlaces}
             options={({ navigation }) => ({
               title: "Your Favorite Places",
-              headerStyle: { backgroundColor: "#f4511e" },
-              headerTintColor: "#fff",
-              headerTitleStyle: { fontWeight: "bold" },
+
               headerRight: ({ tintColor }) => (
                 <IconButton
                   icon="add"
@@ -29,7 +35,13 @@ export default function App() {
               ),
             })}
           />
-          <Stack.Screen name="AddPlace" component={AddPlace} />
+          <Stack.Screen
+            name="AddPlace"
+            component={AddPlace}
+            options={{
+              title: "Add a New Place",
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
